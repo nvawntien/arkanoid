0f39c52bc127a6acd0c2ddf748ff61e96b0c6127
dinhtuanz <24021657@vnu.edu.vn>
2025-10-04 13:19:15 +0700

    tien ngu

--- src/main/java/com/game/arkanoid/models/Paddle.java
+++ src/main/java/com/game/arkanoid/models/Paddle.java
@@ -1,4 +1,65 @@
 package com.game.arkanoid.models;
 
 public class Paddle {
+    private int x;
+    private int y;
+    private int width;
+    private int height;
+    private int speed;
+
+    public Paddle(int x, int y, int width, int height, int speed) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.speed = speed;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void moveLeft() {
+        x -= speed;
+    }
+
+    public void moveRight() {
+        x += speed;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
 }
