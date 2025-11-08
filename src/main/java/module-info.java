2d8699d9aa3320b619a0d21eafdb191ae9b99b45
bmngxn <bmnguyen0906@gmail.com>
2025-11-09 00:05:25 +0700

    match pom.xml

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,16 @@
 module com.game.arkanoid {
+    // --- JavaFX core modules ---
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.media;
     requires javafx.web;
+    requires javafx.swing;
 
+    // --- Standard Java modules ---
+    requires java.sql;
+    requires java.desktop;
+
+    // --- Third-party UI / FXGL frameworks ---
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires net.synedra.validatorfx;
@@ -10,12 +18,25 @@ module com.game.arkanoid {
     requires org.kordamp.bootstrapfx.core;
     requires eu.hansolo.tilesfx;
     requires com.almasb.fxgl.all;
-    requires java.desktop;
+    requires org.postgresql.jdbc;
 
+    // --- External libraries (auto modules) ---
+    // Dotenv auto module
+    requires java.dotenv;
+
+    // --- Exports ---
     exports com.game.arkanoid.app;
     opens com.game.arkanoid.app to javafx.fxml;
+
     exports com.game.arkanoid.controller;
     opens com.game.arkanoid.controller to javafx.fxml;
 
-    requires javafx.media;
+    exports com.game.arkanoid.config;
+    opens com.game.arkanoid.config to javafx.fxml;
+
+    exports com.game.arkanoid.services;
+    opens com.game.arkanoid.services to javafx.fxml;
+
+    exports com.game.arkanoid.models;
+    opens com.game.arkanoid.models to javafx.base;
 }
