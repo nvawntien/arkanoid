2980250bb187760e077e2699f6b8ceea68c7cacb
bmngxn <bmnguyen0906@gmail.com>
2025-11-10 00:30:42 +0700

    add sql queries

---
+++ src/main/resources/com/game/arkanoid/sql/game_state/update_state.sql
@@ -0,0 +1,13 @@
+UPDATE game_states
+SET current_level = ?,
+    score = ?,
+    lives = ?,
+    paddle_x = ?,
+    ball_x = ?,
+    ball_y = ?,
+    bricks = ?::jsonb,
+    powerups = ?::jsonb,
+    balls = ?::jsonb,
+    in_progress = TRUE,
+    updated_at = CURRENT_TIMESTAMP
+WHERE id = ?;
