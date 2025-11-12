f2455250eb0225b6148871bee20803c0a6232851
dinhtuanz <24021657@vnu.edu.vn>
2025-11-12 22:43:32 +0700

    fix map

--- src/main/java/com/game/arkanoid/services/BricksService.java
+++ src/main/java/com/game/arkanoid/services/BricksService.java
@@ -26,7 +26,7 @@ public final class BricksService {
                 int brickHealth = layout[row][col]; 
                 if (brickHealth > 0) {
                     double brickX = col * Constants.BRICK_WIDTH + 22;
-                    double brickY = row * Constants.BRICK_HEIGHT + 172;
+                    double brickY = row * Constants.BRICK_HEIGHT + 250;
 
                     createdBricks.add(new Brick( brickX,  brickY,  Constants.BRICK_WIDTH,  Constants.BRICK_HEIGHT, brickHealth ));
                 }
